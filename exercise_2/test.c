023da9e240f318abf56afdd4fc690096668d20f0
myedgar <sundingcong@msn.com>
2017-01-08 18:03:35 +0800

    i dont konw

---
+++ exercise_2/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  char *str[2] = {"ad","cd"};
+ // str[0] = "first";
+ // str[1] = "second";
+  printf("%s , %s", str[0], str[1]);
+  exit(0);
+}
